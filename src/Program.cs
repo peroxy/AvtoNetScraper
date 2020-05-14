3a806382964e7c825b4ad504d0e09bdb25429210
peroxy </>
2020-05-14 19:54:27 +0200

    Added command line arguments for urls or cars only parsing

--- src/Program.cs
+++ src/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Drawing;
 using System.IO;
+using System.Linq;
 
 namespace AvtoNetScraper
 {
@@ -15,17 +16,48 @@ namespace AvtoNetScraper
         private static void Main(string[] args)
         {
             var settings = Configuration.GetSection(nameof(AppSettings)).Get<AppSettings>();
+            
+            if (args.Length > 2)
+            {
+                Colorful.Console.WriteLine("Invalid arguments supplied, only -urls and -cars allowed.", Color.Red);
+                return;
+            }
+            if (args.Length == 0)
+            {
+                Colorful.Console.WriteLine("Default configuration will be used, scraping car urls first and then car info.", Color.Yellow);
+                ScrapeCarUrls(settings);
+                ScrapeCarInfo(settings);
+            }
+            
+            if (args.Contains("-urls"))
+            {
+                Colorful.Console.WriteLine("-urls option chosen, will scrape car urls from appsettings url values.", Color.Yellow);
+                ScrapeCarUrls(settings);
+            }
+            if (args.Contains("-cars"))
+            {
+                Colorful.Console.WriteLine("-cars option chosen, will scrape car info for all urls in database.", Color.Yellow);
+                ScrapeCarInfo(settings);
+            }
+        }
+
+        private static void ScrapeCarUrls(AppSettings settings)
+        {
             foreach (var url in settings.SearchFilterUrls)
             {
                 var scraper = new PageUrlScraper(url, TimeSpan.FromMilliseconds(settings.RequestIntervalMs));
                 var urls = scraper.GetCarUrls();
-
                 Colorful.Console.WriteLine($"Finished scraping car urls, got {urls.Count} urls.", Color.GreenYellow);
                 var helper = new CarsDbHelper();
-
                 Colorful.Console.WriteLine($"Merging them into database...", Color.LightSkyBlue);
                 helper.MergeUrls(urls);
             }
+        }
+
+        private static void ScrapeCarInfo(AppSettings settings)
+        {
+            //todo: read from db and scrape
+            return;
         }
     }
 }
