2d28b5f6dd2fa308d19d46c8317d08ee66d525f2
MATIC JAZBEC <mj5726@student.uni-lj.si>
2021-02-04 20:24:22 +0100

    fixed scraper

---
+++ src/Migrations/20210203202049_Changelog.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AvtoNetScraper.Migrations
+{
+    public partial class Changelog : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
